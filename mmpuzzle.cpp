f6531453c97f97a85c68195f4f1f51b771e0bd80
Daniel Fuchs <DanFuchs96@gmail.com>
2018-01-27 22:20:50 -0600

    Implemented match function

--- mmpuzzle.cpp
+++ mmpuzzle.cpp
@@ -7,18 +7,33 @@ using namespace std;
 
 MMPuzzle::MMPuzzle(int width, int height, int pool, int num_types)
 {
+    //Assignment
+    m_score = 0;
     m_width = width;
     m_height = height;
     m_pool = pool;
     m_num_types = num_types;
-    vector<int> temp;
+    vector<int> temp1;
+    vector<bool> temp2;
+
+    //Error Correction
+    if(m_width < 3) m_width = 3;
+    if(m_height < 3) m_height = 3;
+    if(m_pool < 0) m_pool = 0;
+    if(m_pool > m_height) { cout << "Error, pool exceeds height. Setting to 1." << endl; m_pool = 1; }
+    if(m_num_types < 2) m_num_types = 2;
+    if(m_num_types > 5) m_num_types = 5;
+
+    //Game-space Initialization
     for(int i = 0; i < height; i++)
     {
-        temp.push_back(0);
+        temp1.push_back(0);
+        temp2.push_back(false);
     }
     for(int i = 0; i < width; i++)
     {
-        m_board.push_back(temp);
+        m_board.push_back(temp1);
+        m_rmv_flags.push_back(temp2);
     }
 }
 
@@ -95,5 +110,63 @@ void MMPuzzle::fall()
         }
     }
     draw(); //DEBUG
+    return;
+}
+
+void MMPuzzle::match()
+{
+    int match_target = -1; //Stores target value for match
+
+    //Part 1: Checking for matches
+    for(int i = 0; i < m_height; i++)
+    {
+        for(int j = 0; j < m_width; j++)
+        {
+            if(!m_rmv_flags[j][i]) //If current part has not been matched
+            {
+                match_target = m_board[j][i]; //Note target match value
+
+                //Part 1.1: Checking for horizontal matches
+                if(j + 2 < m_width) //If enough spaces remain for a potential match
+                {
+                    if(m_board[j+1][i] == match_target && m_board[j+2][i] == match_target) //If sequence of at least 3
+                    {
+                        for(int k = j; k < m_width; k++) //Begin marking parts in match for removal
+                        {
+                            if(m_board[k][i] == match_target) m_rmv_flags[k][i] = true; //Mark part for removal
+                            else break; //End matching sequence
+                        }
+                    }
+                }
+                //Part 1.2: checking for vertical matches
+                if(i + 2 < m_height) //If enough spaces remain for a potential match
+                {
+                    if(m_board[j][i+1] == match_target && m_board[j][i+1] == match_target) //If sequence of at least 3
+                    {
+                        for(int k = i; k < m_height; k++) //Begin marking parts in match for removal
+                        {
+                            if(m_board[j][k] == match_target) m_rmv_flags[j][k] = true; //Mark part for removal
+                            else break; //End matching sequence
+                        }
+                    }
+                }
+                //Match checking complete, move to next part
+            }
+        }
+    }
+
+    //Part 2: Removing Matches
+    for(int i = 0; i < m_height; i++)
+    {
+        for(int j = 0; j < m_width; j++)
+        {
+            if(m_rmv_flags[j][i]) //Part marked for removal
+            {
+                m_board = 0; //Remove part
+                m_rmv_flags[j][i] = false;
+                m_score++; //Record removal
+            }
+        }
+    }
     return;
 }
--- mmpuzzle.h
+++ mmpuzzle.h
@@ -9,9 +9,11 @@ class MMPuzzle
 {
 private:
     vector< vector<int> > m_board; //Board to store parts
-    int m_width; //Width of the board
-    int m_height; //Height of the board
-    int m_pool; //Height of the pool
+    vector< vector<bool> > m_rmv_flags; //Used to mark parts for removal
+    int m_score;     //Stores number of parts removed ("matched")
+    int m_width;     //Width of the board
+    int m_height;    //Height of the board
+    int m_pool;      //Height of the pool
     int m_num_types; //Number of distinct part types
     void drawDivider();
 
@@ -20,6 +22,7 @@ public:
     void setBoard();
     void draw();
     void fall();
+    void match();
 };
 
 #endif
